6ad589043d93d4b8b9becef076328c9bdcecc23c
JotaRibeiro <jota.h.ribeiro@hotmail.com>
2019-04-17 18:07:52 -0300

    Teste no que o Alex fez

--- ep1.py
+++ ep1.py
@@ -11,18 +11,32 @@ def carregar_cenarios():
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
                 "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
+                "biblioteca": "Ir para a biblioteca",
             }
         },
+        "casa": {
+            "titulo": "Humilde residencia",
+            "descricao": "Voce esta em sua casa indeciso sobre seu dia",
+            "opcoes": {
+                "oficina mecanica magrao": "Colocar um escapamento esportivo e deixar a EP de lado",
+                "inicio": "Ir para o insper e negociar sobre a entrega da EP atrasada"
+            }
+        },
+            
         "oficina mecanica magrao": {
             "titulo": "Turbinando o carro",
             "descricao": "Voce esta na oficina mexendo em seu carro antes da entrega da EP",
             "opcoes": {
                 "escapamento novo": "Colocar um escapamento esportivo",
-                "voltar ao INSPER": "Voltar ao INSPER para chegar a tempo para entrega da EP
+                "inicio": "Voltar ao insper para chegar a tempo para entrega da EP"
             }
         },
-        
+        "escapamento novo":{
+            "titulo": "Escapamento ilegal",
+            "descricao": "Voce foi pego pela policia com uma modificacao ilegal e não conseguiu voltar a tempo no INSPER para a entrega da EP",
+            "opcoes": {}
+        },    
+    
         "andar professor": {
             "titulo": "Andar do desespero",
             "descricao": "Voce chegou ao andar da sala do seu professor",
@@ -31,6 +45,7 @@ def carregar_cenarios():
                 "professor": "Falar com o professor"
             }
         },
+        
         "professor": {
             "titulo": "O monstro do Python",
             "descricao": "Voce foi pedir para o professor adiar o EP. "
@@ -38,15 +53,31 @@ def carregar_cenarios():
                          "e devorou sua alma.",
             "opcoes": {}
         },
+        "EP":{
+            "titulo": "A famosa EP meia-boca",
+            "descricao": "Voce fez o trabalho, entregou ao professor, mas ele achou que foi feito nas coxas",
+            "opcoes": {}
+        },
+        
+        "trabalho":{
+            "titulo": "Caverna do desespero",
+            "descricao": "Voce esta na bilbioteca e decidiu tentar fazer o trabalho de ultima hora",
+            "opcoes": {
+                "inicio": "Voltar para o saguao de entrada",
+                "EP": "Fazer o trabalho de ultima hora e tentar ver se o professor aceita"
+            }
+        },
+        
         "biblioteca": {
             "titulo": "Caverna da tranquilidade",
             "descricao": "Voce esta na biblioteca",
             "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
+                "inicio": "Voltar para o saguao de entrada",
+                "trabalho": "Tentar fazer o EP de ultima hora"
             }
         }
     }
-    nome_cenario_atual = "inicio"
+    nome_cenario_atual = "casa"
     return cenarios, nome_cenario_atual
 
 
@@ -58,8 +89,7 @@ def main():
         "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
     print()
     print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
-        "na entrada do Insper, e quer procurar o professor para pedir um "
-        "adiamento do EP (boa sorte...)")
+        "na sua casa, e não sabe se desiste de tudo ou vai para o insper procurar o professor para pedir um adiamento da EP (boa sorte...)")
     print()
 
     cenarios, nome_cenario_atual = carregar_cenarios()
@@ -91,3 +121,5 @@ def main():
 # Programa principal.
 if __name__ == "__main__":
     main()
+
+print("Teste")
